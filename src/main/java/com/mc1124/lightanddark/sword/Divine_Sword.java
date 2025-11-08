2d4f0debf5d99ca431130cc85da10c0b1a5e2b73
dream <2702207128@qq.com>
2025-11-08 16:22:33 +0800

    1

--- src/main/java/com/mc1124/lightanddark/sword/Divine_Sword.java
+++ src/main/java/com/mc1124/lightanddark/sword/Divine_Sword.java
@@ -2,12 +2,18 @@ package com.mc1124.lightanddark.sword;
 
 import com.mc1124.lightanddark.LightAndDarkMod;
 
+import io.netty.util.internal.shaded.org.jctools.queues.MessagePassingQueue.Consumer;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tiers;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import software.bernie.geckolib.constant.DefaultAnimations;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
 
 public class Divine_Sword {
     public static final DeferredRegister<Item> DIVINE_SWORD =
@@ -23,5 +29,13 @@ public class Divine_Sword {
 
         public static void register(IEventBus eventBus) {
         DIVINE_SWORD.register(eventBus);
+    }
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+    controllers.add(new AnimationController<>(this, "Walk/Run/Idle", state -> {
+        if (state.isMoving())
+            return state.setAndContinue(MyEntity.this.isSprinting() ? DefaultAnimations.RUN : DefaultAnimations.WALK);
+
+        return state.setAndContinue(DefaultAnimations.IDLE);
+    }));
     }
 }
